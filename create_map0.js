143992a0d943cfa159beaec3fd80ffec6e0dbb6d
ilyes58 <55973481+ilyes58@users.noreply.github.com>
2020-02-26 16:40:31 +0100

    point ville + nom

--- create_map0.js
+++ create_map0.js
@@ -2,12 +2,12 @@ const width = 700, height = 550;
 
 const path = d3.geoPath();
 
-const projection = d3.geoConicConformal() // Lambert-93
+const projection1 = d3.geoConicConformal() // Lambert-93
     .center([2.454071, 46.279229]) // Center on France
     .scale(2600)
     .translate([width / 2 - 50, height / 2]);
 
-path.projection(projection);
+path.projection(projection1);
 
 const svg = d3.select('#map').append("svg")
     .attr("id", "svg")
@@ -17,13 +17,66 @@ const svg = d3.select('#map').append("svg")
 
 const deps = svg.append("g");
 
+
+function transform(data){
+
+var station = data[0]["station"];
+var result = station.map(function(elt){
+  return {lat : elt.lat,
+          lng : elt.lng,
+          name : elt.n}
+});
+return result;
+};
+
+function drawCircles(data) {
+
+  var div = d3.select("body").append("div")
+      .attr("class", "tooltip")
+      .style("opacity", 0);
+
+  var project = d3.geoConicConformal()
+  .center([2.454071, 46.279229]) // Center on France
+  .scale(2600)
+  .translate([width / 2 - 50, height / 2]);
+
+        svg.selectAll("circle")
+       //3.2 Joignons les données à la sélection
+        .data(data)
+       //3.3 Lions les données à la sélection
+        .join("circle")
+           // Ajoutons les éléments SVG
+
+       //3.4 Modifions les attributs de nos cercles en fonction des données
+           .attr("cy",  function (d) {  return project([d.lng,d.lat])[1]; }) // Le centre du cercle à 60px du haut de mon SVG
+           .attr("cx",  function (d) {  return project([d.lng,d.lat])[0]; })
+           .attr("r", "3px")
+           .attr("fill", "red")
+           .on("mouseover",function affiche(e,i){
+             div.transition()
+                 .duration(200)
+                 .style("opacity", .9);
+             div.html("<b>ville </b>" + e.name + "<br>")
+                 .style("left", (d3.event.pageX + 30) + "px")
+                 .style("top", (d3.event.pageY - 30) + "px");
+           })
+           .on("mouseout", function(d) {
+                   div.style("opacity", 0);
+                   div.html("")
+                       .style("left", "-500px")
+                       .style("top", "-500px");
+                });
+}
+
+
 var promises = [];
 promises.push(d3.json('departments.json'));
 promises.push(d3.csv("population.csv"));
-
+promises.push(d3.json("meteo.json"));
 Promise.all(promises).then(function(values) {
     const geojson = values[0]; // Récupération de la première promesse : le contenu du fichier JSON
     const csv = values[1]; // Récupération de la deuxième promesse : le contenu du fichier csv
+    const metjson = values[2];
 
     var features = deps
         .selectAll("path")
@@ -33,6 +86,18 @@ Promise.all(promises).then(function(values) {
         .attr('id', function(d) {return "d" + d.properties.CODE_DEPT;})
         .attr("d", path);
 
+        result = transform(metjson);
+drawCircles(result);
+
+        // deps
+        // .selectAll("path")
+        // .data(result)
+        // .enter()
+        // .append("circle")
+        // 		.attr("cx", function (d) {  return projection(d)[0]; })
+        // 		.attr("cy", function (d) { return projection(d)[1]; })
+        // 		.attr("r", "8px")
+        // 		.attr("fill", "red");
 
 var quantile = d3.scaleQuantile()
     .domain([0, d3.max(csv, function(e) { return +e.POP; })])
@@ -63,6 +128,29 @@ var div = d3.select("body").append("div")
     .attr("class", "tooltip")
     .style("opacity", 0);
 
+
+// metjson.forEach(function(e,i) {
+//     d3.select()
+//         .attr("class", function(d) { return "department q" + quantile(+e.POP) + "-9"; })
+//         .on("mouseover", function(d) {
+//             div.transition()
+//                 .duration(200)
+//                 .style("opacity", .9);
+//             div.html("<b>Région : </b>" + e.NOM_REGION + "<br>"
+//                     + "<b>Département : </b>" + e.NOM_DEPT + "<br>"
+//                     + "<b>Population : </b>" + e.POP + "<br>")
+//                 .style("left", (d3.event.pageX + 30) + "px")
+//                 .style("top", (d3.event.pageY - 30) + "px");
+//         })
+//         .on("mouseout", function(d) {
+//                 div.style("opacity", 0);
+//                 div.html("")
+//                     .style("left", "-500px")
+//                     .style("top", "-500px");
+//         });
+// });
+
+
 csv.forEach(function(e,i) {
     d3.select("#d" + e.CODE_DEPT)
         .attr("class", function(d) { return "department q" + quantile(+e.POP) + "-9"; })
@@ -76,6 +164,7 @@ csv.forEach(function(e,i) {
                 .style("left", (d3.event.pageX + 30) + "px")
                 .style("top", (d3.event.pageY - 30) + "px");
         })
+
         .on("mouseout", function(d) {
                 div.style("opacity", 0);
                 div.html("")
@@ -83,6 +172,9 @@ csv.forEach(function(e,i) {
                     .style("top", "-500px");
         });
 });
+
+
+
 
 d3.select("select").on("change", function() {
     d3.selectAll("svg").attr("class", this.value);

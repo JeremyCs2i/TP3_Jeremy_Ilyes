20d53aacdc215a5382d9056e8c707b68b916d9b7
ilyes58 <55973481+ilyes58@users.noreply.github.com>
2020-02-26 11:16:58 +0100

    test

--- create_map.js
+++ create_map.js
@@ -5,14 +5,21 @@ const path = d3.geoPath();
 const projection = d3.geoConicConformal() // Lambert-93
     .center([2.454071, 46.279229]) // Center on France
     .scale(2600)
-    .translate([width / 2 - 50, height / 2]);
+    .translate([width / 2, height / 2]);
 
 path.projection(projection);
 
 const svg = d3.select('#map').append("svg")
     .attr("id", "svg")
     .attr("width", width)
-    .attr("height", height)
-    .attr("class", "Blues");
+    .attr("height", height);
 
 const deps = svg.append("g");
+
+d3.json('meteo.json').then(function(geojson) {
+    deps.selectAll("path")
+        .data(geojson.features)
+        .enter()
+        .append("path")
+        .attr("d", path);
+});
